2695de0b563013b773ca6f89b1ad9e2fa1be329a
wangleiei <2350235394@qqcom>
2021-04-03 13:10:45 +0800

    使用自动ACK回复，多次发送失败后自动发送，提高信号稳定 解决模式切换时候会出问题的bug 不要看中文手册，浪费三天时间 记得看芯片和手册是不是一样的

--- Nrf24l01.c
+++ Nrf24l01.c
@@ -4,11 +4,13 @@ extern "C" {
 
 #endif
 #include "Nrf24l01.h"
-static uint8_t NRF_Write_Reg(NRF24L01_STR*base, uint8_t reg, uint8_t value);
-static uint8_t NRF_Read_Reg(NRF24L01_STR*base, uint8_t reg);
-static uint8_t NRF_Write_Buf(NRF24L01_STR*base, uint8_t reg, uint8_t *pBuf, uint8_t uchars);
-static uint8_t NRF_Read_Buf(NRF24L01_STR*base, uint8_t reg, uint8_t *pBuf, uint8_t uchars);
-static void Clear_RX_FIFO(NRF24L01_STR*base);
+static NRF24L01_Mode NRF_Get_Mode(NRF24L01_STR*dev);
+static uint8_t NRF_Get_freq(NRF24L01_STR*dev);
+static uint8_t NRF_Write_Reg(NRF24L01_STR*dev, uint8_t reg, uint8_t value);
+static uint8_t NRF_Read_Reg(NRF24L01_STR*dev, uint8_t reg);
+static uint8_t NRF_Write_Buf(NRF24L01_STR*dev, uint8_t reg, uint8_t *pBuf, uint8_t uchars);
+static uint8_t NRF_Read_Buf(NRF24L01_STR*dev, uint8_t reg, uint8_t *pBuf, uint8_t uchars);
+static void Clear_RX_FIFO(NRF24L01_STR*dev);
 
 
 //***************************************NRF24L01寄存器指令*******************************************************
@@ -47,299 +49,272 @@ static void Clear_RX_FIFO(NRF24L01_STR*base);
 #define FIFO_STATUS     0x17  // FIFO栈入栈出状态寄存器设置
 
 
-//*************************************************************************************************
-// 功能描述 : 写寄存器
-// 输入参数 : reg寄存器地址，value写入的值
-// 返回参数 : status写入后状态
-// 说    明 : 
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-static uint8_t NRF_Write_Reg(NRF24L01_STR*base, uint8_t reg, uint8_t value){
-	uint8_t status;
-	base->nrf24l01csn_l();					      // 选通器件
-	status = base->nrf24l01spirw(value| reg);   
-										  // 写寄存器地址
-	base->nrf24l01spirw(value);		      // 写数据
-	base->nrf24l01csn_h();						  // 禁止该器件
-	return 	status;
-}
-//*************************************************************************************************
-// 功能描述 : 读寄存器
-// 输入参数 : reg寄存器地址
-// 返回参数 : 寄存器值
-// 说    明 : 
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-static uint8_t NRF_Read_Reg(NRF24L01_STR*base, uint8_t reg){
-	uint8_t reg_val;
-	base->nrf24l01csn_l();					  // 选通器件 
-	base->nrf24l01spirw(reg);			      // 写寄存器地址 
-	reg_val = base->nrf24l01spirw(0);	  // 读取该寄存器返回数据 
-	base->nrf24l01csn_h();					  // 禁止该器件 
-  	return 	reg_val;
-}
-
-//*************************************************************************************************
-// 功能描述 : 缓冲区数据写入寄存器
-// 输入参数 : reg寄存器地址，*pBuf待写入数据地址，uchars数据量
-// 返回参数 : 写入状态
-// 说    明 : 
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-static uint8_t NRF_Write_Buf(NRF24L01_STR*base, uint8_t reg, uint8_t *pBuf, uint8_t uchars)
-{
-	uint8_t i;
-	uint8_t status;
-	base->nrf24l01csn_l();				     /* 选通器件 */
-	status = base->nrf24l01spirw(reg);	   /* 写寄存器地址 */
-	for(i=0; i<uchars; i++)
-	{
-		base->nrf24l01spirw(pBuf[i]);		   /* 写数据 */
-	}
-	base->nrf24l01csn_h();						 /* 禁止该器件 */
-  return 	status;	
-}
-
-//*************************************************************************************************
-// 功能描述 : 读寄存器数据存入缓冲区
-// 输入参数 : reg寄存器地址，*pBuf读出的数据暂存地址，uchars数据量
-// 返回参数 : 读取状态
-// 说    明 : 
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-static uint8_t NRF_Read_Buf(NRF24L01_STR*base, uint8_t reg, uint8_t *pBuf, uint8_t uchars)
-{
-	uint8_t i;
-	uint8_t status;
-	base->nrf24l01csn_l();						/* 选通器件 */
-	status = base->nrf24l01spirw(reg);  	/* 写寄存器地址 */
-	for(i=0; i<uchars; i++)
-	{
-		pBuf[i] = base->nrf24l01spirw(0);  /* 读取返回数据 */ 	
-	}
-	base->nrf24l01csn_h();						/* 禁止该器件 */
-  return 	status;
-}
-
-//*************************************************************************************************
-// 功能描述 : 发送缓冲区数据
-// 输入参数 : *tx_buf发送缓冲区地址
-// 返回参数 : Keine
-// 说    明 : 
-// 修改时间 :
-// 修改内容 : 
-//*************************************************************************************************
-uint8_t NRF_TxPacket(NRF24L01_STR*base, uint8_t *tx_buf,uint8_t len)
-{	
-	uint8_t i_u8 = 0;
-	uint8_t Saft_Count_u8 = 0;
-	if(NRF_Get_Mode(base) == NRF_POWER_DOWN){
-		return 1;//掉电模式了
-	}
-	if (len > base->trx_pload_width)
-	{
-		return 1;
-	}
-	NRF_Write_Reg(base,NRFRegSTATUS,0x70);
-	base->nrf24l01ce_l();	
-	NRF_Write_Buf(base,WR_TX_PLOAD, tx_buf, len); // 装载数据
-	base->nrf24l01ce_h();// 置高CE，激发数据发送
-	while((0 == (NRF_Read_Reg(base,NRFRegSTATUS)&0x20))&&(Saft_Count_u8++<100)){//0是没有发送完
-	}
-	if(Saft_Count_u8 < 100){
-		//清位
-		NRF_Write_Reg(base,NRFRegSTATUS,0x20);
-		i_u8  = 0;
-	}else{ 
-		NRF_Write_Reg(base,NRFRegSTATUS,0x70);
-		return 1;
-	}
-	base->nrf24l01ce_l();                                    
-	base->nrf24l01csn_l();                                   
-	base->nrf24l01spirw(FLUSH_TX);// 清空FIFO，关键。不然会出现意想不到的后果！
-	base->nrf24l01csn_h();		                               
-	base->nrf24l01ce_h();	 
-	return i_u8;
-}
-/*NRF接受函数*/
-uint8_t NRF_Receive_Data(NRF24L01_STR*base, uint8_t *ptr,uint8_t len){
-	uint8_t i_u8 = 0;
-	if(NRF_Get_Mode(base) == NRF_POWER_DOWN){
-		return 1;//掉电模式了
-	}
-	if(len<base->trx_pload_width){return 1;}
-	if(0 == (0x40&NRF_Read_Reg(base,NRFRegSTATUS))){
-		return 1;
-	}else{
-		// base->nrf24l01ce_l();
-		NRF_Read_Buf(base,RD_RX_PLOAD,ptr,base->trx_pload_width);
-		NRF_Write_Reg(base,NRFRegSTATUS,0x70);//clear bit
-		base->nrf24l01csn_l();                                   
-		base->nrf24l01spirw(FLUSH_RX);// 清空FIFO，关键。不然会一直认为有接受，数据还是错误的
-		base->nrf24l01csn_h();		                               
-		// base->nrf24l01ce_h();
-		return 0;	
-	}
-}
- 
-/*得到通道频率*/
-uint8_t NRF_Get_freq(NRF24L01_STR*base){
-	return NRF_Read_Reg(base,RF_CH);
-}
-/*得到当前工作模式*/// enum NRF_Mode { NRF_RX,NRF_TX,NRF_STB2,NRF_STB1,NRF_POWER_DOWN };
-uint8_t NRF_Get_Mode(NRF24L01_STR*base){
-	uint8_t te = NRF_Read_Reg(base,CONFIG);
-	uint8_t cepin = base->nrf24l01cesta();
-	if(0 == (te&0x02)){
-		return NRF_POWER_DOWN;
-	}else{
-		if(0 == cepin){
-			return NRF_STB1;
-		}
-		if((1 == cepin)&&(0 == (te&0x01))){
-			return NRF_STB2;
-		}
-		if((1 == cepin)&&(1 == (te&0x01))){
-			return NRF_RX;
-		}
-		return 1;
-	}
-}
-//*************************************************************************************************
-// 功能描述 : 清除接收FIFO
-// 输入参数 : Keine
-// 返回参数 : Keine
-// 说    明 : 发送模式切换为接收模式前先清除接收FIFO 
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-static void Clear_RX_FIFO(NRF24L01_STR*base)    
-{
-	base->nrf24l01ce_l();                                    
-	base->nrf24l01csn_l();                                   
-	base->nrf24l01spirw(FLUSH_RX);                                  // 清空FIFO，关键。不然会出现意想不到的后果！
-	base->nrf24l01csn_h();		                               
-	base->nrf24l01ce_h();
-}
-
-//*************************************************************************************************
-// 功能描述 : 配置成TX_PLOAD_WIDTH Bytes发射数据 
-// 输入参数 : ch 通道号
-// 返回参数 : Keine
-// 说    明 : 发送读卡结果
-// 修改时间 : 2017.5.23
-// 修改内容 : 
-//*************************************************************************************************
-void NRF24L01_TxMode(NRF24L01_STR*base,uint8_t ch)
+void NRF24L01_TxMode(NRF24L01_STR*dev)
 {
 	uint8_t temp_u8;
-	Clear_RX_FIFO(base);
+	dev->nrf_ch = 0xff;
+	Clear_RX_FIFO(dev);
 
-	base->nrf24l01ce_l();	
-	// base->nrf24l01delayms(1);
-	NRF_Write_Buf(base,TX_ADDR,base->nrf_addr,5);      // 写寄存器指令+接收地址使能指令+接收地址+地址宽度
-	NRF_Write_Buf(base,RX_ADDR_P0,base->nrf_addr,5);// 为了应答接收设备，接收通道0地址和发送地址相同	
-	NRF_Write_Reg(base,EN_AA,0);       //全部是普通模式，发送出去，就不管了 方便	
-	// base->nrf24l01delayms(1);
-	// temp_u8 = NRF_Read_Reg(base,EN_AA);
-	NRF_Write_Reg(base,EN_RXADDR,0x01);   // 使能接收通道0
+	dev->nrf24l01ce_l();	
 
-	NRF_Write_Reg(base,SETUP_RETR,0x0);	 //全部是普通模式，发送出去，就不管了 方便	
+	NRF_Write_Buf(dev,TX_ADDR,dev->nrf_addr,5);      // 写寄存器指令+接收地址使能指令+接收地址+地址宽度
+	NRF_Write_Buf(dev,RX_ADDR_P0,dev->nrf_addr,5);// 为了应答接收设备，接收通道0地址和发送地址相同	
+	NRF_Write_Reg(dev,EN_AA,0);       //全部是普通模式，发送出去，就不管了 方便	
 
-	NRF_Write_Reg(base,RF_CH,ch);         // 选择射频通道
+	// temp_u8 = NRF_Read_Reg(dev,EN_AA);
+	NRF_Write_Reg(dev,EN_RXADDR,0x01);   // 使能接收通道0
 
-	NRF_Write_Reg(base,RX_PW_P0,base->trx_pload_width);							 // 接收通道0选择和发送通道相同有效数据宽度
+	NRF_Write_Reg(dev,SETUP_RETR,0x0);	 //全部是普通模式，发送出去，就不管了 方便	
+
+	NRF_Write_Reg(dev,RF_CH,2);         // 选择射频通道 固定通道2
+
+	NRF_Write_Reg(dev,RX_PW_P0,dev->trx_pload_width);							 // 接收通道0选择和发送通道相同有效数据宽度
 
 	temp_u8 = 0;
-	if (base->nrf_air_rate == 2)
+	if (dev->nrf_air_rate == 2)
 	{
 		temp_u8 |= 0x08;
-	}else if(((base->nrf_air_rate == 1))||((base->nrf_air_rate == 250)))
+	}else if(((dev->nrf_air_rate == 1))||((dev->nrf_air_rate == 250)))
 	{
 		temp_u8 |= 0x20;
 	}
 
-  	if (base->nrf_trans_power == -12)
+  	if (dev->nrf_trans_power == -12)
 		temp_u8 |= 0x02;
-	else if (base->nrf_trans_power == -6)
+	else if (dev->nrf_trans_power == -6)
 		temp_u8 |= 0x04;
-	else if (base->nrf_trans_power == 0)
+	else if (dev->nrf_trans_power == 0)
 		temp_u8 |= 0x06;
 
-  	NRF_Write_Reg(base,RF_SETUP,temp_u8);    	
-	NRF_Write_Reg(base,CONFIG,0x7E);      // CRC使能，16位CRC校验，上电
-	if (base->enable_int == 1)
+  	NRF_Write_Reg(dev,RF_SETUP,temp_u8);    	
+	NRF_Write_Reg(dev,CONFIG,0x7E);      // CRC使能，16位CRC校验，上电
+	if (dev->enable_int == 1)
 	{
-		NRF_Write_Reg(base,CONFIG,0x42);
+		NRF_Write_Reg(dev,CONFIG,0x42);
 	}else
 	{
-		NRF_Write_Reg(base,CONFIG,0x72);
+		NRF_Write_Reg(dev,CONFIG,0x72);
 	}
 	
-	base->nrf24l01csn_l();                                   
-	base->nrf24l01spirw(FLUSH_TX);                              // 用于清空FIFO，不然会出现意想不到的后果！ 
-	base->nrf24l01csn_h();
-	base->nrf24l01delayms(1);
-	base->nrf24l01ce_h();	 	
-	base->nrf24l01delayms(700);
+	dev->nrf24l01csn_l();                                   
+	dev->nrf24l01spirw(FLUSH_TX);// 用于清空FIFO，不然会出现意想不到的后果！ 
+	dev->nrf24l01csn_h();
+	// dev->nrf24l01delayms(1);//!!!!
+	dev->nrf24l01ce_h();	 	
+	// dev->nrf24l01delayms(700);//!!!
+	dev->nrf_ch = NRF_Get_freq(dev);
+
+	dev->Nrf24l01Mode = NRF24_TX;
 }
 
-//*************************************************************************************************
-// 功能描述 : 接收指令函数
-// 输入参数 : ch 通道号
-// 返回参数 : Keine
-// 说    明 : 接收动作遥控指令专用
-// 修改时间 : 
-// 修改内容 : 
-//*************************************************************************************************
-void NRF24L01_RxMode(NRF24L01_STR*base,uint8_t ch)
+void NRF24L01_RxMode(NRF24L01_STR*dev)
 {
 	uint8_t temp_u8;
-	base->nrf24l01ce_l();	
-	base->nrf24l01delayms(1);
-	NRF_Write_Buf(base,RX_ADDR_P0,base->nrf_addr,5);// 为了应答接收设备，接收通道0地址和发送地址相同	
-	NRF_Write_Reg(base,EN_AA,0);       //全部是普通模式，发送出去，就不管了 方便	
+	dev->nrf_ch = 0xff;
+	dev->nrf24l01ce_l();	
+	//dev->nrf24l01delayms(1);
+	NRF_Write_Buf(dev,RX_ADDR_P0,dev->nrf_addr,5);// 为了应答接收设备，接收通道0地址和发送地址相同	
+	NRF_Write_Reg(dev,EN_AA,0);       //全部是普通模式，发送出去，就不管了 方便	
 
-	NRF_Write_Reg(base,EN_RXADDR,0x01);   // 使能接收通道0 
+	NRF_Write_Reg(dev,EN_RXADDR,0x01);   // 使能接收通道0 
 
-	NRF_Write_Reg(base,RF_CH,ch);         // 选择射频通道
+	NRF_Write_Reg(dev,RF_CH,2);         // 选择射频通道 固定通道2
 
-	NRF_Write_Reg(base,RX_PW_P0,base->trx_pload_width);// 接收通道0选择和发送通道相同有效数据宽度
+	NRF_Write_Reg(dev,RX_PW_P0,dev->trx_pload_width);// 接收通道0选择和发送通道相同有效数据宽度
 
 	temp_u8 = 0;
-  	if(base->nrf_air_rate == 2){
+  	if(dev->nrf_air_rate == 2){
 		temp_u8 |= 0x08;
   	}
 	else{
 		temp_u8 |= 0x20;
 	}
-  	if (base->nrf_trans_power == -12){
+  	if (dev->nrf_trans_power == -12){
 		temp_u8 |= 0x02;
   	}
-	else if (base->nrf_trans_power == -6){
+	else if (dev->nrf_trans_power == -6){
 		temp_u8 |= 0x04;
 	}
-	else if (base->nrf_trans_power == 0){
+	else if (dev->nrf_trans_power == 0){
 		temp_u8 |= 0x06;
 	}
-	NRF_Write_Reg(base,RF_SETUP,temp_u8);    // 数据传输率1Mbps，发射功率0dBm，低噪声放大器增益	
-	NRF_Write_Reg(base,CONFIG,0x77);      //，上电 	
-	if(base->enable_int == 1){
-		NRF_Write_Reg(base,CONFIG,0x33);
+	NRF_Write_Reg(dev,RF_SETUP,temp_u8);    // 数据传输率1Mbps，发射功率0dBm，低噪声放大器增益	
+	NRF_Write_Reg(dev,CONFIG,0x77);      //，上电 	
+	if(dev->enable_int == 1){
+		NRF_Write_Reg(dev,CONFIG,0x33);
 	}else{
-		NRF_Write_Reg(base,CONFIG,0x73);
+		NRF_Write_Reg(dev,CONFIG,0x73);
 	}
-	base->nrf24l01csn_l();                                   
-	base->nrf24l01spirw(FLUSH_RX);// 用于清空FIFO ！！关键！！不然会出现意想不到的后果！！！大家记住！！ 
-	base->nrf24l01csn_h();		                               
+	dev->nrf24l01csn_l();                                   
+	dev->nrf24l01spirw(FLUSH_RX);// 用于清空FIFO ！！关键！！不然会出现意想不到的后果！！！大家记住！！ 
+	dev->nrf24l01csn_h();		                               
 	
-	base->nrf24l01delayms(1);
-	base->nrf24l01ce_h();	
+	//dev->nrf24l01delayms(1);
+	dev->nrf24l01ce_h();	
+	dev->nrf_ch = NRF_Get_freq(dev);
+
+	dev->Nrf24l01Mode = NRF24_RX;
 }
+
+void NrfInit(NRF24L01_STR*dev,
+	uint8_t (*nrf24l01spirw)(uint8_t data),
+	void (*nrf24l01csn_h)(void),
+	void (*nrf24l01csn_l)(void),
+	void (*nrf24l01ce_h)(void),
+	void (*nrf24l01ce_l)(void),
+	uint8_t addr,
+	uint8_t enable_int
+	){
+	dev->nrf_addr[0] = 0x23;
+	dev->nrf_addr[1] = 0x45;
+	dev->nrf_addr[2] = 0x11;
+	dev->nrf_addr[3] = 0xae;
+	dev->nrf_addr[4] = addr;
+	dev->nrf_air_rate = 2;
+	dev->nrf_trans_power = 0;
+	dev->trx_pload_width = 8;
+	dev->enable_int = enable_int;
+	dev->nrf_ch = 2;
+
+	dev->nrf24l01spirw = nrf24l01spirw;
+	dev->nrf24l01csn_h = nrf24l01csn_h;
+	dev->nrf24l01csn_l = nrf24l01csn_l;
+	dev->nrf24l01ce_h = nrf24l01ce_h;
+	dev->nrf24l01ce_l = nrf24l01ce_l;
+
+	dev->Nrf24l01Mode = NRF24_IDLE;
+}
+
+// -2:发送超时，无法发送
+// -1：处于掉电模式，需要初始化
+// 0：长度超过发送长度
+// >0:实际发送长度
+int8_t NRF_TxPacket(NRF24L01_STR*dev, uint8_t *tx_buf,uint8_t len)
+{	
+	uint8_t Saft_Count_u8 = 0;
+	if(NRF_Get_Mode(dev) == NRF24_POWER_DOWN){
+		return -1;//掉电模式了
+	}
+	if (len > dev->trx_pload_width)
+	{
+		return 0;
+	}
+	NRF_Write_Reg(dev,NRFRegSTATUS,0x70);
+	dev->nrf24l01ce_l();	
+	NRF_Write_Buf(dev,WR_TX_PLOAD, tx_buf, len); // 装载数据
+	dev->nrf24l01ce_h();// 置高CE，激发数据发送
+	while((0 == (NRF_Read_Reg(dev,NRFRegSTATUS)&0x20))&&(Saft_Count_u8++<100)){//0是没有发送完
+	}
+	if(Saft_Count_u8 < 100){
+		//清位
+		NRF_Write_Reg(dev,NRFRegSTATUS,0x20);
+	}else{ 
+		NRF_Write_Reg(dev,NRFRegSTATUS,0x70);
+		return -2;
+	}
+	dev->nrf24l01ce_l();                                    
+	dev->nrf24l01csn_l();                                   
+	dev->nrf24l01spirw(FLUSH_TX);// 清空FIFO，关键。不然会出现意想不到的后果！
+	dev->nrf24l01csn_h();		                               
+	dev->nrf24l01ce_h();	 
+	return dev->trx_pload_width;
+}
+// -2:接受长度超过能提供的长度
+// -1:无法接受数据，处于掉电模式
+// 0：没有收到数据
+// >0：数据长度
+int8_t NRF_Receive_Data(NRF24L01_STR*dev, uint8_t *ptr,uint8_t len){
+	uint8_t i_u8 = 0;
+	if(NRF_Get_Mode(dev) == NRF24_POWER_DOWN){
+		return -1;//掉电模式了
+	}
+	if(len > dev->trx_pload_width){
+		return -2;
+	}
+	if(0 == (0x40&NRF_Read_Reg(dev,NRFRegSTATUS))){
+		return 0;
+	}else{
+
+		NRF_Read_Buf(dev,RD_RX_PLOAD,ptr,dev->trx_pload_width);
+		NRF_Write_Reg(dev,NRFRegSTATUS,0x70);//clear bit
+		dev->nrf24l01csn_l();                                   
+		dev->nrf24l01spirw(FLUSH_RX);// 清空FIFO，关键。不然会一直认为有接受，数据还是错误的
+		dev->nrf24l01csn_h();		                               
+
+		return dev->trx_pload_width;
+	}
+}
+static uint8_t NRF_Write_Reg(NRF24L01_STR*dev, uint8_t reg, uint8_t value){
+	uint8_t status;
+	dev->nrf24l01csn_l();					      // 选通器件
+	status = dev->nrf24l01spirw(NRF_WRITE_REG | reg);   
+										  // 写寄存器地址
+	dev->nrf24l01spirw(value);		      // 写数据
+	dev->nrf24l01csn_h();						  // 禁止该器件
+	return 	status;
+}
+static uint8_t NRF_Read_Reg(NRF24L01_STR*dev, uint8_t reg){
+	uint8_t reg_val;
+	dev->nrf24l01csn_l();					  // 选通器件 
+	dev->nrf24l01spirw(NRF_READ_REG | reg);			      // 写寄存器地址 
+	reg_val = dev->nrf24l01spirw(0);	  // 读取该寄存器返回数据 
+	dev->nrf24l01csn_h();					  // 禁止该器件 
+  	return 	reg_val;
+}
+
+static uint8_t NRF_Write_Buf(NRF24L01_STR*dev, uint8_t reg, uint8_t *pBuf, uint8_t uchars)
+{
+	uint8_t i;
+	uint8_t status;
+	dev->nrf24l01csn_l();				     /* 选通器件 */
+	status = dev->nrf24l01spirw(NRF_WRITE_REG | reg);	   /* 写寄存器地址 */
+	for(i=0; i<uchars; i++)
+	{
+		dev->nrf24l01spirw(pBuf[i]);		   /* 写数据 */
+	}
+	dev->nrf24l01csn_h();						 /* 禁止该器件 */
+  return 	status;	
+}
+static uint8_t NRF_Read_Buf(NRF24L01_STR*dev, uint8_t reg, uint8_t *pBuf, uint8_t uchars)
+{
+	uint8_t i;
+	uint8_t status;
+	dev->nrf24l01csn_l();						/* 选通器件 */
+	status = dev->nrf24l01spirw(NRF_READ_REG | reg);  	/* 写寄存器地址 */
+	for(i=0; i<uchars; i++)
+	{
+		pBuf[i] = dev->nrf24l01spirw(0);  /* 读取返回数据 */ 	
+	}
+	dev->nrf24l01csn_h();						/* 禁止该器件 */
+  return 	status;
+}
+/*得到通道频率*/
+static uint8_t NRF_Get_freq(NRF24L01_STR*dev){
+	return NRF_Read_Reg(dev,RF_CH);
+}
+/*得到当前工作模式*///
+static NRF24L01_Mode NRF_Get_Mode(NRF24L01_STR*dev){
+	uint8_t te = NRF_Read_Reg(dev,CONFIG);
+	// uint8_t cepin = dev->nrf24l01cesta();
+	if(0 == (te&0x02)){
+		return NRF24_POWER_DOWN;
+	}else{
+		if(te&0x01){
+			return NRF24_RX;
+		}else{
+			return NRF24_STB2;
+		}
+	}
+}
+static void Clear_RX_FIFO(NRF24L01_STR*dev)    
+{
+	dev->nrf24l01ce_l();                                    
+	dev->nrf24l01csn_l();                                   
+	dev->nrf24l01spirw(FLUSH_RX);                                  // 清空FIFO，关键。不然会出现意想不到的后果！
+	dev->nrf24l01csn_h();		                               
+	dev->nrf24l01ce_h();
+}
+
 #ifdef __cplusplus
 }
 #endif
--- Nrf24l01.h
+++ Nrf24l01.h
@@ -8,31 +8,55 @@ extern "C" {
 
 #include <stdint.h>
 
+typedef enum NRF24L01_Mode { NRF24_RX,NRF24_TX,NRF24_STB2,NRF24_STB1,NRF24_POWER_DOWN,NRF24_IDLE}NRF24L01_Mode;
 typedef struct NRF24L01_STR{
 
-	uint8_t (*nrf24l01spirw)(uint8_t data);/*spi要求，时钟平时为低，在下降沿传输数据能读能写*/
-	void (*nrf24l01delayms)(uint32_t data);
+	// int(*nrf24l01cesta)(void);
+	// 固定成10字节//只能是8？
+	uint8_t trx_pload_width;//发送数据最大字节长度，8-64 
+	// 固定0
+	int8_t nrf_trans_power;//发送功率最大值-18->-18dBm -12->-12dBm -6->-6dBm 0->0dBm 可选有0，-6，-12，-18
+	// 固定2
+	uint8_t nrf_air_rate;//发送载波频率2M和1M,25khz 可选有1和2 250
+	
+	uint8_t enable_int;//使用中断吗，1表示使用接受中断，其他值表示不使用中断
+	// 固定 0x23,0x45,0x11,0xae,0x??
+	uint8_t nrf_addr[5];//nrf地址，5个字节
+	// 固定2
+	uint8_t nrf_ch;
+
+	NRF24L01_Mode Nrf24l01Mode;
+	/*spi要求，时钟平时为低，在下降沿传输数据能读能写*/
+	uint8_t (*nrf24l01spirw)(uint8_t data);
+	void (*nrf24l01delayms)(uint32_t data);//可以不用
 	void (*nrf24l01csn_h)(void);
 	void (*nrf24l01csn_l)(void);
 	void (*nrf24l01ce_h)(void);
 	void (*nrf24l01ce_l)(void);
-	int(*nrf24l01cesta)(void);
-	uint8_t trx_pload_width;//发送数据最大字节长度，8-64
-	int8_t nrf_trans_power;//发送功率最大值-18->-18dBm -12->-12dBm -6->-6dBm 0->0dBm 可选有0，-6，-12，-18
-	uint8_t nrf_air_rate;//发送载波频率2M和1M,25khz 可选有1和2 250
-	uint8_t enable_int;//使用中断吗，1表示使用接受中断，其他值表示不使用中断
-	uint8_t nrf_addr[5];//nrf地址，5个字节
-	
 }NRF24L01_STR;
-enum NRF_Mode { NRF_RX,NRF_TX,NRF_STB2,NRF_STB1,NRF_POWER_DOWN };
+void NrfInit(NRF24L01_STR*dev,
+	uint8_t (*nrf24l01spirw)(uint8_t data),
+	void (*nrf24l01csn_h)(void),
+	void (*nrf24l01csn_l)(void),
+	void (*nrf24l01ce_h)(void),
+	void (*nrf24l01ce_l)(void),
+	uint8_t addr,
+	uint8_t enable_int
+	);
 
+// -2:接受长度超过能提供的长度
+// -1:无法接受数据，处于掉电模式
+// 0：没有收到数据
+// >0：数据长度
+int8_t NRF_Receive_Data(NRF24L01_STR*dev, uint8_t *ptr,uint8_t len);
+// -2:发送超时，无法发送
+// -1：处于掉电模式，需要初始化
+// 0：长度超过发送长度
+// >0:实际发送长度
+int8_t NRF_TxPacket(NRF24L01_STR*dev, uint8_t *tx_buf,uint8_t len);
 
-uint8_t NRF_TxPacket(NRF24L01_STR*base, uint8_t *tx_buf,uint8_t len);
-uint8_t NRF_Receive_Data(NRF24L01_STR*base, uint8_t *ptr,uint8_t len);
-// uint8_t NRF_Get_freq(NRF24L01_STR*base);
-// uint8_t NRF_Get_Mode(NRF24L01_STR*base);
-void NRF24L01_TxMode(NRF24L01_STR*base,uint8_t ch);
-void NRF24L01_RxMode(NRF24L01_STR*base,uint8_t ch);
+void NRF24L01_TxMode(NRF24L01_STR*dev);
+void NRF24L01_RxMode(NRF24L01_STR*dev);
 
 //*************************************指令宏定义*******************************************
 #endif
